daf6fea1560d37aba95fb2e9a42fb64499b44be9
myojin223 <64243555+myojin223@users.noreply.github.com>
2022-01-02 23:47:00 -0700

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Render All Powershell Script
  Script for Windows Powershell to create proxies from multiple templates, rename them and add them to a completed folder.
+
+# Change the template names to match what your template is called.
---
+++ Render All Normal (OG Set Symbols).ps1
@@ -0,0 +1,47 @@
+function ProxyRename {
+    Param(
+        [Parameter(Mandatory, Position = 0)][string]$Template
+    )
+    
+    $srcpath = ".\images\fronts\" # Path to Proximtiy Output Images with trailing \
+    $dstpath = ".\Completed\" # Path to folder for completed, renamged images with trailing \
+
+    $srclist = Get-ChildItem -Path $srcpath
+
+    foreach ($srcimage in $srclist) {
+        if ($Template -eq "NONE") {
+            $dstimage = $srcimage.name -replace "^\d+ (.*).png","`$1.png"
+        }
+        else {
+            $dstimage = $srcimage.name -replace "^\d+ (.*).png","`$1 ($Template).png"
+        }
+        $srcimage = $srcpath + $srcimage.name
+        $dstimage = $dstpath + $dstimage
+        Move-Item "$srcimage" "$dstimage" -Force
+    }
+
+}
+
+java -jar proximity.jar --cards=Decklist.txt --art_source=LOCAL --reminder_text=true --template=M15 --copyright=false --use_card_back=false
+ProxyRename "NONE"
+
+java -jar proximity.jar --cards=Decklist.txt --art_source=LOCAL --reminder_text=true --template=FANGEXT --copyright=false --use_card_back=false
+ProxyRename "FANG Extended"
+
+java -jar proximity.jar --cards=Decklist.txt --art_source=LOCAL --reminder_text=true --template=FANG --copyright=false --use_card_back=false
+ProxyRename "FANG"
+
+java -jar proximity.jar --cards=Decklist.txt --art_source=LOCAL --reminder_text=true --template=M10 --copyright=false --use_card_back=false
+ProxyRename "M10"
+
+java -jar proximity.jar --cards=Decklist.txt --art_source=LOCAL --reminder_text=true --template=SSDFC --copyright=false --use_card_back=false
+ProxyRename "SSDFC"
+
+java -jar proximity.jar --cards=Decklist.txt --art_source=LOCAL --reminder_text=true --template=MaleMPC --copyright=false --use_card_back=false
+ProxyRename "MaleMPC"
+
+java -jar proximity.jar --cards=Decklist.txt --art_source=LOCAL --reminder_text=true --template=Extended --copyright=false --use_card_back=false
+ProxyRename "Extended"
+
+java -jar proximity.jar --cards=Decklist.txt --art_source=LOCAL --reminder_text=true --template=PHYREXIAN --copyright=false --use_card_back=false
+ProxyRename "Phyrexian"
